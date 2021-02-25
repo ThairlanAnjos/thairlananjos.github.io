f563d2a5c27de8da9f848cb84c5d578372010934
inthayrlan <hello.thay@outlook.com>
2021-02-25 14:18:47 -0300

    Add a pinterest tag script

--- scripts.js
+++ scripts.js
@@ -34,7 +34,7 @@
               };
               var
                 n = window.pintrk;
-                 n.queue = [], n.version = "3.0";
+                git git  n.queue = [], n.version = "3.0";
               var
                 t = document.createElement("script");
               t.async = !0, t.src = e;
