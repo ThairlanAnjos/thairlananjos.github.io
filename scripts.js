8f59a0e0aac1ecf8a7b98e74d84e7912baafcc9d
ThairlanAnjos <thairlananjos@oulook.com>
2021-11-04 23:43:58 -0300

    Update links in menu

--- scripts.js
+++ scripts.js
@@ -1,26 +1,26 @@
-/* Redirect to Mobile*/
-
-
-/*
-if (screen.width <= 699) {
-  document.location = "https://m.thairlananjos.com";
-}
-*/
-
 /* Google Tag Manager */
 
 (function (w, d, s, l, i) {
   w[l] = w[l] || [];
   w[l].push({
-    'gtm.start': new Date().getTime(),
-    event: 'gtm.js'
+    "gtm.start": new Date().getTime(),
+    event: "gtm.js",
   });
   var f = d.getElementsByTagName(s)[0],
     j = d.createElement(s),
-    dl = l != 'dataLayer' ? '&l=' + l : '';
+    dl = l != "dataLayer" ? "&l=" + l : "";
   j.async = true;
-  j.src =
-    'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
   f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-PRMR4JZ');
+})(window, document, "script", "dataLayer", "GTM-PRMR4JZ");
 
+/* -------------- Navbar ---------------- */
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
