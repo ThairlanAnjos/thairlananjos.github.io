d66102828d14bc1f0d561acfc4279ca9a34de45a
thairlananjos <thairlananjos@gmail.com>
2024-03-24 19:23:23 +0000

    add dev nix file

---
+++ .idx/dev.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }: {
+
+# NOTE: This is an excerpt of a complete Nix configuration example.
+# For more information about the dev.nix file in IDX, see
+# https://developers.google.com/idx/guides/customize-idx-env
+
+# Enable previews and customize configuration
+idx.previews = {
+  enable = true;
+  previews = [
+    # The following object sets web previews
+    {
+      command = [
+        "npm"
+        "run"
+        "start"
+        "--"
+        "--port"
+        "$PORT"
+        "--host"
+        "0.0.0.0"
+        "--disable-host-check"
+      ];
+      id = "web";
+      manager = "web";
+    }
+    # The following object sets Android previews
+    # Note that this is supported only on FLutter workspaces
+
+    {
+      id = "android";
+      manager = "flutter";
+    }
+  ];
+};
